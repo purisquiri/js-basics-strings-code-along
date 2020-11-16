3d24ee7ade0735008df172d4ee51b610241df317
purisquiri <mau.dorigo@outlook.es>
2020-11-15 20:22:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+const shortGreeting = welcomeMessage.slice(0,7)+", "+currentUser.slice(0,1)+'!'
